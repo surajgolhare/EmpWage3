cbf8b551fd044481e4ec108982bc8879648b877e
unknown <you@example.com>
2020-06-03 11:13:30 +0530

    Emp present or absent

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,1 +1,11 @@
-echo "Welcome to github"
+#!/bin/bash -x
+echo "Welcome to Git bash"
+echo "Welcome to employee Wage computation Program"
+isPresent=$(( RANDOM % 2 ))
+if [ $isPresent -eq 1 ]
+then
+        echo "present"
+elif [ $isPresent -eq 0 ]
+then
+        echo "Absent"
+fi
